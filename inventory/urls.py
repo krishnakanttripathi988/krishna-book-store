e7456e4e885502fc1e60afe20f2ab9b070cd9114
Krishna Kant Tripathi <54737485+krishnakanttripathi988@users.noreply.github.com>
2022-03-25 16:55:45 +0530

    Update urls.py

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name= 'inventory'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('/', views.IndexView.as_view(), name='index'),
     path('inventory', views.InventoryView.as_view(), name= 'inventory'),
     path('<str:ID>/edit', views.BooksUpdate.as_view(), name='books_edit'),
     path('<str:ID>/delete', views.BooksDelete.as_view(), name= 'books_delete'),
